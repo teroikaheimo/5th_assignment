fa916b9af26ffd5d6b91f50afbeb1a465b96ec2c
teroikaheimo <tero.ikaheimo@gmail.com>
2019-10-04 11:48:21 +0300

    Changed textView to listView. JSON array fetching now works. Listing  dates to listView works.

--- app/src/main/java/com/example/a5thassignment_http/MainActivity.java
+++ app/src/main/java/com/example/a5thassignment_http/MainActivity.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,9 +23,18 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /*
 * Create a user interface which consists of a ListView which covers the whole screen and button at
@@ -44,46 +57,91 @@ Try to find a way to deserialize, the response (parse/convert) into a list of yo
 * */
 
 public class MainActivity extends AppCompatActivity {
-RequestQueue requestQueue;
-    StringRequest stringRequest;
-    String url;
-    TextView textView;
+    // Views
     Button buttonGet;
+    ListView listView;
+
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
+
+    // Request HTTP
+    RequestQueue requestQueue;
+    StringRequest stringRequest;
+    JsonArrayRequest jsonArrayRequest;
+    String url;
     ConnectivityManager connMan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Views
-        textView = findViewById(R.id.textView);
+        listView = findViewById(R.id.listView);
         buttonGet = findViewById(R.id.button);
-        buttonGet.setOnClickListener(new View.OnClickListener() {
 
+        // Button functionality
+        buttonGet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 addGetRequestToQueue();
             }
         });
 
-        connMan = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        arrayList = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+        //// Request
+        // Returns JSON data
         url = "https://webd.savonia.fi/home/ktkoiju/j2me/test_json.php?dates&delay=1";
+        connMan = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
         requestQueue = Volley.newRequestQueue(this);
+
+
+        jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+                    // TODO checkout Gson. Googles own tool.
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        for (Integer i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject row = response.getJSONObject(i);
+                                arrayAdapter.add(row.getString("pvm"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        arrayAdapter.notifyDataSetChanged();
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Query failed...\n\r" + error.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+/*
         stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
-                        textView.setText("Response is: " + response.substring(0, 500));
+                        arrayList.add(response);
+                        arrayAdapter.notifyDataSetChanged();
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(), "Query failed...\n\r" + error.getMessage(), Toast.LENGTH_LONG).show();
             }
-        });
+        });*/
     }
 
     private void addGetRequestToQueue() {
-        requestQueue.add(stringRequest);
+        requestQueue.add(jsonArrayRequest);
     }
 }
