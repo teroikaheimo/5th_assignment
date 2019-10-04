bfe693ae4d0e504dbea6cad461b755d980dc6ade
teroikaheimo <tero.ikaheimo@gmail.com>
2019-10-04 10:38:57 +0300

    Added a button and temporary textview to display data that is fetched. Added toast to display error. Fetching data now works.

--- app/src/main/java/com/example/a5thassignment_http/MainActivity.java
+++ app/src/main/java/com/example/a5thassignment_http/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.a5thassignment_http;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -13,6 +19,9 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /*
 * Create a user interface which consists of a ListView which covers the whole screen and button at
@@ -36,11 +45,45 @@ Try to find a way to deserialize, the response (parse/convert) into a list of yo
 
 public class MainActivity extends AppCompatActivity {
 RequestQueue requestQueue;
-
+    StringRequest stringRequest;
+    String url;
+    TextView textView;
+    Button buttonGet;
+    ConnectivityManager connMan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Views
+        textView = findViewById(R.id.textView);
+        buttonGet = findViewById(R.id.button);
+        buttonGet.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                addGetRequestToQueue();
+            }
+        });
+
+        connMan = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        url = "https://webd.savonia.fi/home/ktkoiju/j2me/test_json.php?dates&delay=1";
         requestQueue = Volley.newRequestQueue(this);
+        stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Query failed...\n\r" + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void addGetRequestToQueue() {
+        requestQueue.add(stringRequest);
     }
 }
